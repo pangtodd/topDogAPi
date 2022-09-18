18286151d5f248e9f1b71b2b033b007d44479a2f
Todd Pangilinan <pang.todd@gmail.com>
2022-09-18 10:28:41 -0700

    set up useEffect tomake API call.

--- src/components/TopDogs.js
+++ src/components/TopDogs.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TopDogs(){
   const [error, setError]= useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [topDogs, setTopDogs]= useState([]);
+
+  useEffect(() => {
+    fetch(`localhost:3000/pets`)
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+          setTopStories(jsonifiedResponse.results)
+          setIsLoaded(true)
+        })
+      .catch((error) => {
+        setError(error)
+        setIsLoaded(true)
+      });
+    }, [])
 }
+
 
 export default TopDogs;
