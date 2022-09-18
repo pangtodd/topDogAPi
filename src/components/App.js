0c291591a6465f64a270673dd712fd714c28c9c6
Todd Pangilinan <pang.todd@gmail.com>
2022-09-18 10:12:25 -0700

    established initial App.js.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TopDogs from './TopDogs';
+
+function App(){
+  return(
+    <React.Fragment>
+      < TopDogs />
+    </React.Fragment>
+  );
+}
+
+
+export default App;
