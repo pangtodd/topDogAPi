c0f591c21f10689c9a8dd0bf76b669145966e7a6
Todd Pangilinan <pang.todd@gmail.com>
2022-09-18 10:16:08 -0700

    establish inital TopDogs.js file.

---
+++ src/components/TopDogs.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function TopDogs(){
+  const [error, setError]= useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [topDogs, setTopDogs]= useState([]);
+}
+
+export default TopDogs;
